ef433a87862a4774bcdc47e876dc3e6658a61ac0
Gabriel <gabriel.avila.ga09@gmail.com>
2025-08-01 15:21:42 -0500

    Initialized Spring Security Project

---
+++ securityexample/src/test/java/com/springexample/securityexample/SecurityexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springexample.securityexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityexampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
